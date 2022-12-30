d4feeec0d2485ab3129fb796e145673f3d383305
Mateus Macedo <mateusouza2014@live.com>
2022-12-30 18:15:56 -0300

    feat: Adicionando estrutura Models

--- src/ImpulsionaTech.Contas.Domain/Models/TiposContas/TipoConta.cs
+++ src/ImpulsionaTech.Contas.Domain/Models/TiposContas/TipoConta.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImpulsionaTech.Contas.Domain.Models.Clientes;
+using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using ImpulsionaTech.Contas.Domain.Base;
 
 namespace ImpulsionaTech.Contas.Domain.Models.TiposContas
 {
